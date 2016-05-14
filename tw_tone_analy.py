787983a604509c0ba8cb90fa204cba725200b661
Mike Sallese <msallese31@gmail.com>
2016-05-14 15:13:19 -0400

    add watson tone analyzer service and limit trends to 5

--- tw_tone_analy.py
+++ tw_tone_analy.py
@@ -1,6 +1,7 @@
 import twitter
 import json
 import sys
+from watson_developer_cloud import ToneAnalyzerV3Beta
 
 CONSUMER_KEY = 'xcgIGah9GQmug62fCupl8f8Vk'
 CONSUMER_SECRET = sys.argv[1]
@@ -10,6 +11,14 @@ OAUTH_TOKEN_SECRET = sys.argv[2]
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 twitter_api = twitter.Twitter(auth=auth)
+
+tone_analyzer = ToneAnalyzerV3Beta(
+    username='e6ac8a83-d333-4002-b9db-3208fc9189d7',
+    password=sys.argv[3],
+    version='2016-02-11'
+)
+
+print(json.dumps(tone_analyzer.tone(text='I am very happy'), indent=2))
 
 US_WOE_ID = 23424977
 us_trends = twitter_api.trends.place(_id=US_WOE_ID)
@@ -25,6 +34,15 @@ tw_json = json.loads(tw)
     #            print trends['name']
 
 #print "**********************************"
+i = 0
+trends_list = []
 for et in tw_json:
     for trends in et["trends"]:
-        print trends["name"]
+        if(i > 4):
+            break
+        else:
+            i += 1
+            trends_list.append(trends["name"])
+print trends_list
+for trend in trends_list:
+    print trend
